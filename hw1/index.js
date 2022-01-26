6a9f5c867f20646d726793b76459923dd911ed21
Jeff <jeffjr25@icloud.com>
2022-01-26 08:29:47 -0500

    hwcomplete

--- hw1/index.js
+++ hw1/index.js
@@ -1,11 +1,53 @@
-import React from 'react'
-
-function index() {
-  return (
-    <div>
-      
-    </div>
-  )
+Array.prototype.map = function (cb) {
+  let cArr = [...this]
+  let newArr = []
+  for(let i = 0; i < cArr.length; i++) {
+      newArr.push(cb(cArr[i], i, cArr))
+  }
+  return newArr
 }
 
-export default index
+//filter function implement
+Array.prototype.filter = function (a) {
+  let cArr = [...this]
+  let newArr = []
+  for(let i = 0; i < cArr.length; i++) {
+      let resp = a(cArr[i], i, cArr)
+      if(resp) {
+          newArr.push(cArr[i])
+      }
+  }
+  return newArr
+}
+
+//reduce function implement
+Array.prototype.reduce = function (a, num) {
+  this.num = num === undefined ? 0 : num
+  let sum = this.num
+  for(let i = 0; i < this.length; i++) {
+      sum = a(sum, this[i], i, this)
+  }
+  return sum
+}
+
+//some function implement 
+Array.prototype.some = function (a) {
+  for(let i = 0; i < this.length; i++) {
+      resp = a(this[i], i, this)
+      if(resp) {
+          return true
+      }
+  }
+  return false
+}
+
+//every function implement 
+Array.prototype.every = function (a) {
+  for(let i = 0; i < this.length; i++) {
+      resp = a(this[i], i, this)
+      if(!res) {
+          return false
+      }
+  }
+  return true
+}
